a4210e05c9c82b44eaa6206ce5914f17cb0448a3
Animeki004 <hindianimeki@gmail.com>
2026-05-27 14:31:38 +0530

    updated.

--- gemini_client/enums.py
+++ gemini_client/enums.py
@@ -14,7 +14,7 @@ class Endpoint(Enum):
     INIT = "https://gemini.google.com/app"
     GENERATE = "https://gemini.google.com/_/BardChatUi/data/assistant.lamda.BardFrontendService/StreamGenerate"
     ROTATE_COOKIES = "https://accounts.google.com/RotateCookies"
-    UPLOAD = "https://content-push.googleapis.com/upload"
+    UPLOAD = "https://push.clients6.google.com/upload"
 
 class Headers(Enum):
     """
